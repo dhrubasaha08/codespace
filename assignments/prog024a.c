ce3fce1c05c9a350c6c9c4ccb71d6f7a8275c67e
dhrubasaha08 <dhrubasaha@outlook.com>
2022-06-13 23:19:09 +0530

    13/6/22

--- assignments/prog024a.c
+++ assignments/prog024a.c
@@ -12,10 +12,9 @@ Date:           13/06/2022
 
 
 void inputArr(int *arr, int n);
+void maxHeapify(int *arr, int n, int i);
+void buildMaxHeap(int *arr, int n);
 void printArr(int *arr, int n);
-void heapify(int *arr, int n, int i);
-void heapSort(int *arr, int n);
-
 
 void main()
 {
@@ -40,6 +39,84 @@ void main()
     printArr(arr, n);
 
     free(arr);
+}
+
+
+void inputArr(int *arr, int n)
+{
+    int i;
+
+    printf("Enter the elements:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+}
+
+
+void printArr(int *arr, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+}
+
+
+void heapify(int *arr, int n, int i)
+{
+    int largest, left, right;
+
+    left = 2 * i + 1;
+    right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[i])
+    {
+        largest = left;
+    }
+    else
+    {
+        largest = i;
+    }
+
+    if (right < n && arr[right] > arr[largest])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
+    }
+}
+
+
+void heapSort(int *arr, int n)
+{
+    int i;
+
+    for (i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    for (i = n - 1; i >= 0; i--)
+    {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
 }
 
 /*
