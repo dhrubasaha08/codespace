a8124c9ca8bdf1f8629be11a58eed650b312829f
dhrubasaha08 <dhrubasaha@outlook.com>
2022-06-28 05:09:21 +0530

    28/6/22

--- practical/prog015.c
+++ practical/prog015.c
@@ -4,3 +4,58 @@ Roll No:        B.Sc(Sem-IV)-04
 Program No:     15
 Program Name:   Write a C program to implement prim's algorithm.
 ***************************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define INF 9999999
+#define V 5
+
+int G[V][V] = {
+    {0, 9, 75, 0, 0},
+    {9, 0, 95, 19, 42},
+    {75, 95, 0, 51, 66},
+    {0, 19, 51, 0, 31},
+    {0, 42, 66, 31, 0}};
+
+int main()
+{
+    int no_edge, selected[V], x, y;
+
+    memset(selected, false, sizeof(selected));
+    no_edge = 0;
+    selected[0] = true;
+    printf("Edge : Weight\n");
+
+    while (no_edge < V - 1)
+    {
+        int min = INF;
+        x = 0;
+        y = 0;
+
+        for (int i = 0; i < V; i++)
+        {
+            if (selected[i])
+            {
+                for (int j = 0; j < V; j++)
+                {
+                    if (!selected[j] && G[i][j])
+                    {
+                        if (min > G[i][j])
+                        {
+                            min = G[i][j];
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
+            }
+        }
+        printf("%d - %d : %d\n", x, y, G[x][y]);
+        selected[y] = true;
+        no_edge++;
+    }
+
+    return 0;
+}
