5205604dac5860b356b1e9f91f697d24903f697e
dhrubasaha08 <dhrubasaha08@gmail.com>
2022-12-16 10:05:12 +0530

    code added

---
+++ webd/js/3.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')();
+
+let n,i,flag=0
+
+n = prompt('Enter number :');
+
+if (n == 0 || n == 1) {
+    flag = 1;
+}
+
+for (i = 2; i <= n/2; ++i) {
+    if (n % i == 0) {
+        flag = 1;
+        break;
+    }
+}
+
+if (flag == 0) {
+    console.log(`Given number ${n} is a prime number`);
+} else {
+    console.log(`Given number ${n} is a non-prime number`);
+}
